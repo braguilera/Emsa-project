372b84f59e749bd6bff481afaee01ca7ad8548fe
braguilera <braaguileraa@gmail.com>
2025-05-27 15:23:16 -0300

    Componente de titulos realizado

--- src/screens/Services.jsx
+++ src/screens/Services.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Title from '../components/utils/Title'
 
 const Services = () => {
   return (
-    <div>
-        Services
-    </div>
+    <section className='h-screen'>
+        <Title backgroundText={'Servicios'} frontText={'Nuestros Servicios'}></Title>
+    </section>
   )
 }
 
