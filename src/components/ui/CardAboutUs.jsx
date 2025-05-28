077312be82af91af24f7a6c1c5c577d00e6993de
braguilera <braaguileraa@gmail.com>
2025-05-28 19:23:51 -0300

    Pantalla de certificado lista

--- src/components/ui/CardAboutUs.jsx
+++ src/components/ui/CardAboutUs.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CardAboutUs = ({us, key}) => {
+const CardAboutUs = ({us}) => {
   return (
-    <article key={key} className="p-6 mb-4 flex w-full md:w-1/2 flex-col">
+    <article className="p-6 mb-4 flex w-full md:w-1/2 flex-col">
         <h2 className="text-xl text-secondary font-bold mb-4 text-center">{us.title}</h2>
         <div className='w-1/3 bg-secondary h-1'></div>
         <p className="text-gray-700 text-base mt-4">{us.text}</p>
--- src/screens/Certificate.jsx
+++ src/screens/Certificate.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import linesDeco from '../assets/lines_deco.png'
+import { useTranslation } from 'react-i18next'
 
 const Certificate = () => {
+    const { t } = useTranslation('certificate')
+
   return (
-    <div>
-      Certificate
-    </div>
+    <section className='flex py-4 flex-col w-full xl:pl-96 pl-20 sm:pl-40 lg:pl-60 h-20 sm:h-40 lg:h-60 xl:h-96 justify-center sm: relative'>
+      <img src={linesDeco} alt='Decoración con lineas' className='absolute left-0 top-0 h-full'/>
+      <h1 className='text-primary font-bold text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>{t('title')}</h1>
+      <p className='w-1/2 xl:w-1/3 text-sm md:text-base lg:text-lg xl:text-xl'>{t('description')}</p>
+    </section>
   )
 }
 
